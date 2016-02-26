6315d9fde4e5586c58905e1bd17bbbda82484fbf
KozikR <rafkozik@gmail.com>
2016-02-26 14:22:18 +0100

    use 8 bit bus

--- use_xillybus.c
+++ use_xillybus.c
@@ -7,36 +7,30 @@
 #include <fcntl.h>
 #include <stdint.h>
 
+#include <string.h>
+
 int main(int argc, char *argv[]) {
 
   int fdr, fdw;
 
-  struct {
-    uint32_t v1;
-    float v2;
-  } tologic, fromlogic;
-
-  fdr = open("/dev/xillybus_read_32", O_RDONLY);
-  fdw = open("/dev/xillybus_write_32", O_WRONLY);
+  fdr = open("/dev/xillybus_read_8", O_RDONLY);
+  fdw = open("/dev/xillybus_write_8", O_WRONLY);
 
   if ((fdr < 0) || (fdw < 0)) {
     perror("Failed to open Xillybus device file(s)");
     exit(1);
   }
 
-  tologic.v1 = 123;
-  tologic.v2 = 0.78539816; // ~ pi/4
-
   // Not checking return values of write() and read(). This must be done
   // in a real-life program to ensure reliability.
+  
+  unsigned char data_w[] = "Ala ma kota";
+  unsigned char data_r[20];
 
-  write(fdw, (void *) &tologic, sizeof(tologic));
-  read(fdr, (void *) &fromlogic, sizeof(fromlogic));
+  write(fdw, (void *) data_w, strlen(data_w)+1);
+  read(fdr, (void *) data_r, strlen(data_w)+1);
 
-  printf("FPGA said: %d + 1 = %d and also "
-	 "sin(%f) = %f\n",
-	 tologic.v1, fromlogic.v1,
-	 tologic.v2, fromlogic.v2);
+  printf("Send: %s; Receive %s\n", data_w, data_r);
 
   close(fdr);
   close(fdw);
