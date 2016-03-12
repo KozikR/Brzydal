621b6bc704c49dc455ac50919dae5c7edc186faa
KozikR <rafkozik@gmail.com>
2016-03-12 17:33:51 +0100

    Bug fix

--- inc/simpleFace.h
+++ inc/simpleFace.h
@@ -1,6 +1,8 @@
 #ifndef SIMPLEFACE_H
 #define SIMPLEFACE_H
 
+#include "face.h"
+
 class SimpleFace : public Face{
     SimpleFace();
     
--- src/simpleFace.cpp
+++ src/simpleFace.cpp
@@ -1,6 +1,7 @@
 #include "simpleFace.h"
 
-SimpleFace::SimpleFace(){    
+SimpleFace::SimpleFace()
+{    
     circle(image, Point(350, 240), 200, Scalar(255,255,255), 5);
     
     circle(image, Point(300, 190), 20, Scalar(255,255,255), 2);
