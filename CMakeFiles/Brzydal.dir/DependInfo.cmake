78e5794269826d5d0512b29f4480ff9ebf95edad
root <root@localhost.localdomain>
2016-03-12 12:01:37 +0000

    Fix CMAKE errors

---
+++ CMakeFiles/Brzydal.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Brzydal/src/face.cpp" "/root/Brzydal/CMakeFiles/Brzydal.dir/src/face.cpp.o"
+  "/root/Brzydal/src/main.cpp" "/root/Brzydal/CMakeFiles/Brzydal.dir/src/main.cpp.o"
+  "/root/Brzydal/src/servo.cpp" "/root/Brzydal/CMakeFiles/Brzydal.dir/src/servo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CMakeFiles/Brzydal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Brzydal.dir/src/face.cpp.o"
+  "CMakeFiles/Brzydal.dir/src/main.cpp.o"
+  "CMakeFiles/Brzydal.dir/src/servo.cpp.o"
+  "Brzydal.pdb"
+  "Brzydal"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Brzydal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
